b753c400263956fe662c716e96887428cc5f288b
Divyanshu <divyanshu202507@gmail.com>
2025-10-01 02:08:17 +0530

    1-10

--- src/Top.jsx
+++ src/Top.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import svg from './assets/logout.svg'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { TbLogout } from "react-icons/tb";
 
 import './Top.css';
 
@@ -11,7 +12,7 @@ const Top = () => {
         <div><h1>Planora.ai</h1></div>
         <div className='right-section'>
           <h4>Admin</h4>
-          <button> <h3>Logout</h3> <img src={svg} alt="icon" className='img1' /></button>
+          <button className='logoutbtn'> <h3>Logout</h3><TbLogout className='logout'/></button>
           </div>
       </header>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,26 +10,79 @@ const Home = () => {
         setNotices(prev => [...prev, message]);
     };
 
-    const handleclick = () => {
-       window.open("C:/Users/shash/OneDrive/Desktop/Madhur Ka Past.pdf", "_blank");    
-    }
+
 
     return (
         <>
             <h2 className='heading2'>My Today Classes</h2>
             <br />
             <div className='classes'>
-                {[1, 2, 3, 4, 5].map((num) => (
-                    <div key={num} className='infocls-wrapper'>
-                        <div className='infocls'>
-                            <p>Class {num}</p>
-                        </div>
-                        <div className='optionstimetable'>
-                            <button onClick={() => handleCancelClass(num)}>Cancel Class</button>
-                            <button onClick={() => alert(`Change timings for class ${num}`)}>Change Timings</button>
-                        </div>
+
+                <div id='1' className='infocls-wrapper'>
+                    <div className='infocls'>
+                        <h4 className='subcode'>MHIC-101</h4>
+                        <p className='time'>09:00 AM - 10:00 AM</p>
+                        <p className='room'>Room 101</p>
+                        <p className='branch'>Micro. VLSI</p>
                     </div>
-                ))}
+                    <div className='optionstimetable'>
+                        <button onClick={() => handleCancelClass(num)}>Cancel Class</button>
+                        <button onClick={() => alert(`Change timings for class ${num}`)}>Change Timings</button>
+                    </div>
+                </div>
+
+                <div id='2' className='infocls-wrapper'>
+                    <div className='infocls'>
+                        <h4 className='subcode'>MHIC-101</h4>
+                        <p className='time'>10:00 AM - 11:00 AM</p>
+                        <p className='room'>Room 104 </p>
+                        <p className='branch'>Robotics</p>
+                    </div>
+                    <div className='optionstimetable'>
+                        <button onClick={() => handleCancelClass(num)}>Cancel Class</button>
+                        <button onClick={() => alert(`Change timings for class ${num}`)}>Change Timings</button>
+                    </div>
+                </div>
+
+                <div id='3' className='infocls-wrapper'>
+                    <div className='infocls'>
+                        <h4 className='subcode'>MHIC-101</h4>
+                        <p className='time'>11:00 AM - 12:00 AM</p>
+                        <p className='room'>Room 108</p>
+                        <p className='branch'>ECE</p>
+                    </div>
+                    <div className='optionstimetable'>
+                        <button onClick={() => handleCancelClass(num)}>Cancel Class</button>
+                        <button onClick={() => alert(`Change timings for class ${num}`)}>Change Timings</button>
+                    </div>
+                </div>
+
+                <div id='4' className='infocls-wrapper'>
+                    <div className='infocls'>
+                        <h4 className='subcode'>MHIC-101</h4>
+                        <p className='time'>02:00 PM - 03:00 PM</p>
+                        <p className='room'>Room 107</p>
+                        <p className='branch'>CSE</p>
+                    </div>
+                    <div className='optionstimetable'>
+                        <button onClick={() => handleCancelClass(num)}>Cancel Class</button>
+                        <button onClick={() => alert(`Change timings for class ${num}`)}>Change Timings</button>
+                    </div>
+                </div>
+
+                <div id='5' className='infocls-wrapper'>
+                    <div className='infocls'>
+                        <h4 className='subcode'>MHIC-101</h4>
+                        <p className='time'>04:00 PM - 05:00 PM</p>
+                        <p className='room'>Room 105</p>
+                        <p className='branch'>Electrical Eng.</p>
+                    </div>
+                    <div className='optionstimetable'>
+                        <button onClick={() => handleCancelClass(num)}>Cancel Class</button>
+                        <button onClick={() => alert(`Change timings for class ${num}`)}>Change Timings</button>
+                    </div>
+                </div>
+
             </div>
 
             <br />
@@ -39,11 +92,23 @@ const Home = () => {
                 <h3 className='notifi'>New Notifications</h3>
                 <br />
 
-                <div  onClick={handleclick} className='noti1'></div>
-                <div className='noti1'></div>
-                <div className='noti1'></div>
-                <div className='noti1'></div>
-                <div className='noti1'></div>
+                <div className='noti1'>
+                    <a className='a1' href='https://nitkkr.ac.in/wp-content/uploads/2025/08/Notification-Regarding-Internal-Reappear-Examination-for-AugSept-2025-Examination-Session.pdf' target="_blank" rel="noopener noreferrer">Notification Regarding UG/PG Internal Reappear Examination for Nov/Dec-2025 Examination Session</a>
+                </div>
+                <div className='noti1'>
+                    <a className='a1' href='https://nitkkr.ac.in/wp-content/uploads/2025/09/Revised-Notification-Schedule-of-Mid-Sem-Exam-I-18.09.2025.pdf' target="_blank" rel="noopener noreferrer">Revised Notification and time slots for Mid Semester Exam-I for Nov./Dec.-2025 Examination Session</a>
+                </div>
+                <div className='noti1'>
+                    <a className='a1' href='https://docs.google.com/forms/d/e/1FAIpQLSfqgGy7NauzYunvNx0ESsdvwqqxSEdRzRczTF9TVL3nOGzrfA/viewform' target="_blank" rel="noopener noreferrer">The Department of Computer Applications, NIT Kurukshetra, Haryana is going to jointly organize FDP on Recent Advancements in Deep Learning Architectures (RADA-2025) with E & ICT Academy IIIT Jabalpur in Online mode during November 13-19, 2025.</a>
+                </div>
+                <div className='noti1'>
+                    <a className='a1' href='https://nitkkr.ac.in/wp-content/uploads/2025/08/Notification-Regarding-Internal-Reappear-Examination-for-AugSept-2025-Examination-Session.pdf' target="_blank" rel="noopener noreferrer">Notification Regarding UG/PG Internal Reappear Examination for Nov/Dec-2025 Examination Session</a>
+                </div>
+                <div className='noti1'>
+                    <a className='a1' href='https://nitkkr.ac.in/wp-content/uploads/2025/09/Detailed-Advertisement-Advt.No_.23.2025.pdf' target="_blank" rel="noopener noreferrer">Recruitment of Junior Research Fellows (JRFs) for the MeitY, Govt. of India sponsored project</a>
+                </div>
+
+                <a href='#' className='viewall'>View All Notifications</a>
 
             </div>
 
